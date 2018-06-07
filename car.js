0293274fac7927bffb243f0721782f412c82c20a
Tomasz Rekawek <tomasz.rekawek@rank.com>
2018-06-07 17:30:19 +0200

    add more tests

--- car.js
+++ car.js
@@ -1,5 +1,25 @@
 class car {
     constructor() {
+        this.speed = 0;
+        this.wheel = 0;
+    }
+    checkSpeed() {
+        return this.speed;
+    }
+    turnWheel(angle) {
+        this.wheel = angle;
+    }
+    break(howStrong, howLong) {
+        this.speed = this.speed - howStrong * howLong;
+    }
+    accelerate(howFast, howLong) {
+        if(typeof howFast !== 'number' || typeof howLong !== 'number') {
+            throw 'accelerate works only with numbers'
+        }
+        this.speed = this.speed + howFast * howLong;
+    }
+    stop() {
+        this.speed = 0;
     }
 }
 export default car;
--- car.test.js
+++ car.test.js
@@ -1,15 +1,34 @@
 import car from './car.js';
 describe('car interface', () => {
+    let myCar = new car();
     test('it should be able to accelerate', () => {
-        expect(car.accelerate).toBeDefined();
+        expect(myCar.accelerate).toBeDefined();
     });
     test('it should be able to break', () => {
-        expect(car.break).toBeDefined();
+        expect(myCar.break).toBeDefined();
     });
     test('it should have turnWheel method', () => {
-        expect(car.turnWheel).toBeDefined();
+        expect(myCar.turnWheel).toBeDefined();
     });
     test('it should have speedometer', () => {
-        expect(car.checkSpeed).toBeDefined()
+        expect(myCar.checkSpeed).toBeDefined()
     });
+
+    test('it should have emergancy breaking method', () => {
+        expect(myCar.stop).toBeDefined();
+    });
+});
+
+
+describe('accelerating', () => {
+    test('speed should be 100km when accelereting 100ms^2 for 1 sec', () => {
+        let myCar = new car();
+        myCar.accelerate(100, 1);
+        expect(myCar.checkSpeed()).toEqual(100);
+    })
+    test('car should throw an error when passed non numerical arugments to accelerate', () => {
+        let myCar = new car();
+        expect(() => {myCar.accelerate('zupa', '13')}).toThrowError();
+        expect(() => {myCar.accelerate(12, 'dd')}).toThrowError();
+    })
 });
